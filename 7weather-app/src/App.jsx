cc82931dcfcd5452454ee7387b560f676e251ed9
Sabiha Manzoor <sabihamanzoor3@gmail.com>
2026-08-22 11:28:17 -0700

    Update App.jsx

--- 7weather-app/src/App.jsx
+++ 7weather-app/src/App.jsx
@@ -1,7 +1,9 @@
+import { useState } from 'react'
 import './App.css';
 import Weather from './components/weather';
 
 function App() {
+  const [count, setCount] = useState(0)
   return (
     <div className="App">
      <Weather/>
